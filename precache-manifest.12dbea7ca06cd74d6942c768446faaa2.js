a03c3079eb17357a0d5f449179f54e507355da23
onlycan17 <onlycan17@gmail.com>
2022-02-07 20:38:45 +0900

    Updates

---
+++ precache-manifest.12dbea7ca06cd74d6942c768446faaa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e6951c25d1feeeb3ab68909a429a0d0",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "68b542d49425ac2e6937",
+    "url": "/nwitter/static/css/main.f81b29c5.chunk.css"
+  },
+  {
+    "revision": "712d0dbb7c4fac16d8cb",
+    "url": "/nwitter/static/js/2.30371c82.chunk.js"
+  },
+  {
+    "revision": "beedd4aa548457aec04236a3c132a394",
+    "url": "/nwitter/static/js/2.30371c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b542d49425ac2e6937",
+    "url": "/nwitter/static/js/main.9c2c2ec5.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
